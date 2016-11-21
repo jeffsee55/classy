84e53ce972c06b4d99698bed7baa7493037a40e2
= <=>
2016-11-21 12:38:41 -0500

    Adds header layout

--- views/layout/header.blade.php
+++ views/layout/header.blade.php
@@ -1,11 +1,18 @@
 <header>
-	<nav>
-		<ul>
-			@if ($items = $menu->get_items())
-				@foreach ($items as $item)
-					<li><a href="{{ $item->get_link() }}">{{ $item->get_title() }}</a></li>
-				@endforeach
-			@endif
-		</ul>
+	<nav class="nav has-shadow">
+		<div class="container">
+			<div class="nav-left">
+				@if ($items = $menu->get_items())
+					@foreach ($items as $item)
+						<a class="nav-item is-tab" href="{{ $item->get_link() }}">{{ $item->get_title() }}</a>
+						@if (!empty($item->get_children()))
+							@foreach ($item->get_children() as $child_item)
+								<a class="nav-item is-tab" href="{{ $child_item->get_link() }}">{{ $child_item->get_title() }}</a>
+							@endforeach
+						@endif
+					@endforeach
+				@endif
+			</div>
+		</div>
 	</nav>
 </header>
--- views/post/archive.blade.php
+++ views/post/archive.blade.php
@@ -1,8 +1,6 @@
 @extends('layout.default')
 
 @section('content')
-	<h1>{{ $page_title }}</h1>
-
 	@if (isset($posts))
 		@forelse ($posts as $post)
 			@include ('post.preview')
@@ -12,5 +10,5 @@
 	@endif
 
 	@include ('layout.pagination')
-	
+
 @stop
