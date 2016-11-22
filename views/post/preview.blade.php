4bf2d724cd327ad1d63fdb5970b29ca778d44eb6
= <=>
2016-11-21 19:54:41 -0500

    eh

--- views/post/preview.blade.php
+++ views/post/preview.blade.php
@@ -1,15 +1,15 @@
 <section class="section preview">
 	<article class="level columns">
-		<div class="column is-half">
-			<div class="box box-left">
+		<figure class="column is-half">
+			<img src="{{ $post->thumbnail()->src('large') }}" alt="">
+		</figure>
+		<div class="preview-text column is-half">
+			<div class="box box-right">
 				<label class="category">{{ $post->get_category() }}</label>
 				<h1 class="title is-large"><a href="{{ $post->permalink() }}">{{ $post->title() }}</a></h1>
 				<date>{{ $post->get_date() }}</date>
 			</div>
 			<p>{{ $post->get_preview() }}</p>
 		</div>
-		<figure class="column is-half">
-			<img src="{{ $post->thumbnail()->src('large') }}" alt="">
-		</figure>
 	</article>
 </section>
