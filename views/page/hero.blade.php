265968e69c415b3084c4c37eb14a496ba088a5f1
= <=>
2017-01-19 21:14:10 -0500

    Updats hero for categorie

--- views/page/hero.blade.php
+++ views/page/hero.blade.php
@@ -5,7 +5,7 @@
 
 @section('content')
 
-    <section class="hero is-large {{ $post->maybe_overlay() }}" style="background-image: url({{ $post->thumbnail()->src('wide') }})">
+    <section class="home-hero hero is-large {{ $post->maybe_overlay() }}" style="background-image: url({{ $post->thumbnail()->src('wide') }})">
     	<div class="hero-body">
     		<div class="container {{ $post->title_position() }}">
     			<div class="column is-half">
