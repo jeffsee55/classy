e50c284b18d4bd4dda65202895ad3b0fe6e49a40
Jeff See <jeffsee.55@gmail.com>
2017-02-18 03:58:44 -0500

    Switches images to background images for index

--- views/post/preview.blade.php
+++ views/post/preview.blade.php
@@ -1,11 +1,12 @@
 <section class="section preview {{ $post->get_layout() }}">
 	<article class="columns is-gapless">
-		<figure class="column is-half">
-			<a href="{{ $post->get_permalink() }}">
-				<img src="{{ $post->thumbnail()->src('large') }}" alt="">
-			</a>
-		</figure>
-		<div class="column is-half">
+		<div class="image-column column is-5">
+			<figure style="background-image: url({{ $post->thumbnail()->src('large') }})">
+				<a href="{{ $post->get_permalink() }}">
+				</a>
+			</figure>
+		</div>
+		<div class="text-column column is-7">
 			<div class="preview-text">
 				<div class="text-wrapper">
 					@include ('post.partials.box')
